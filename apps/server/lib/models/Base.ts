bc586667ae0db9d4c882e4a7da1b075bfe4ab565
Vlad Zusko <vladzvu11@gmail.com>
2022-07-10 00:13:10 +0300

    feat: add models

---
+++ apps/server/lib/models/Base.ts
@@ -0,0 +1,45 @@
+import Sequelize, { ModelAttributes, ModelStatic, Transaction, WhereOptions } from 'sequelize';
+import { uuid, TransactionOptions } from '../types/common';
+import { throwError } from '../types/error';
+
+export default class Base<T, K> extends Sequelize.Model<T, K> {
+    declare createdAt: Date;
+
+    declare updatedAt: Date;
+
+    public constructor() {
+        super();
+    }
+
+    static sequelizeTimeStampFields = {
+        createdAt : { type: Sequelize.DATE, allowNull: false },
+        updatedAt : { type: Sequelize.DATE, allowNull: false },
+        deletedAt : { type: Sequelize.DATE, allowNull: true }
+    };
+
+    static async findById<N extends Sequelize.Model>(this: ModelStatic<N>, id: uuid, opts?: { transaction: Transaction})
+        : Promise<N | null> {
+        return this.findByPk(id, opts);
+    }
+
+    async validateFieldUnique<N extends Sequelize.Model>(
+        this: N, model: ModelStatic<N>, field: keyof ModelAttributes<N>, value: any,
+        { transaction }: TransactionOptions
+    ) {
+        if (!field) return;
+
+        if (this.getDataValue(field) === value) return;
+
+        console.log(this.constructor.name);
+        const existingEmail = await model.findOne({
+            where : {
+                [field] : value
+            } as WhereOptions,
+            transaction
+        });
+
+        if (existingEmail) throwError('NOT_UNIQUE', 'email');
+
+        return;
+    }
+}
---
+++ apps/server/lib/models/User.ts
@@ -0,0 +1,34 @@
+import Sequelize from 'sequelize';
+import { user, common } from '../types';
+import { sequelize } from '../db';
+import Base from './Base';
+
+
+export default class User extends Base<user.IUser, user.IUserCreate> {
+    declare id: common.uuid;
+
+    declare email: string;
+
+    declare username: string;
+
+    declare password: string;
+
+    declare firstname: string;
+
+    declare lastname: string;
+}
+
+
+User.init({
+    id        : { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
+    email     : { type: Sequelize.STRING, unique: true, allowNull: false },
+    username  : { type: Sequelize.STRING, unique: true, allowNull: false },
+    password  : { type: Sequelize.STRING, allowNull: false },
+    firstname : { type: Sequelize.STRING, allowNull: true },
+    lastname  : { type: Sequelize.STRING, allowNull: true },
+
+    ...User.sequelizeTimeStampFields
+}, {
+    paranoid : true,
+    sequelize
+});

28e1fa262de1f6bc10059db92200104f4e312691
Vlad Zusko <vladzvu11@gmail.com>
2022-07-10 00:16:15 +0300

    feat: add db

---
+++ apps/server/lib/db.ts
@@ -0,0 +1,42 @@
+import { Sequelize } from 'sequelize';
+import config from './config';
+
+const { dev, test } = config.db;
+
+let POSTGRES_USER;
+
+let POSTGRES_PASSWORD;
+
+let POSTGRES_HOST;
+
+let POSTGRES_PORT;
+
+let POSTGRES_NAME;
+
+if (!config.isTest) {
+    POSTGRES_USER = dev.user;
+    POSTGRES_PASSWORD = dev.password;
+    POSTGRES_HOST = dev.host;
+    POSTGRES_PORT = dev.port;
+    POSTGRES_NAME = dev.name;
+} else {
+    POSTGRES_USER = test.user;
+    POSTGRES_PASSWORD = test.password;
+    POSTGRES_HOST = test.host;
+    POSTGRES_PORT = test.port;
+    POSTGRES_NAME = test.name;
+}
+
+const connectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_NAME}`;
+
+export const sequelize = new Sequelize(connectionString, {
+    logging : config.isTest ? false : console.log
+});
+
+const db = {
+    Sequelize,
+    sequelize
+};
+
+
+export default db;

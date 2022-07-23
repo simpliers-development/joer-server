3b1ccdc8d591fbe1c42bce52cea75664d9ac4c41
Vasylenko Paul <maincrafter1307@gmail.com>
2022-07-23 16:32:53 +0300

    feat: readme

--- apps/server/migrations/20220703162148-User.js
+++ apps/server/migrations/20220703162148-User.js
@@ -8,10 +8,10 @@ module.exports = {
             await queryInterface.createTable('Users', {
                 id        : { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
                 email     : { type: Sequelize.STRING, unique: true, allowNull: false },
-                username  : { type: Sequelize.STRING, unique: true, allowNull: false },
+                userName  : { type: Sequelize.STRING, unique: true, allowNull: false },
                 password  : { type: Sequelize.STRING, allowNull: false },
-                firstname : { type: Sequelize.STRING, allowNull: true },
-                lastname  : { type: Sequelize.STRING, allowNull: true },
+                firstName : { type: Sequelize.STRING, allowNull: true },
+                lastName  : { type: Sequelize.STRING, allowNull: true },
                 createdAt : { type: Sequelize.DATE, allowNull: false },
                 updatedAt : { type: Sequelize.DATE, allowNull: false },
                 deletedAt : { type: Sequelize.DATE, allowNull: true }
